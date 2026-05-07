f67dfb4ae751ebc7fff86b873d8cc93db2be27eb
Russu Ion <irussu46@gmail.com>
2026-05-08 02:00:39 +0300

    Connect with database

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db")
+const dotenv = require("dotenv")
 
+
+dotenv.config();
+connectDB();
 const app = express();
-
+const PORT = process.env.PORT || 5000
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Server merge!" });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
